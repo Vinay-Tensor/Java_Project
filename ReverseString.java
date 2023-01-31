f069843842abaf14ef156818e6467a9577c5d43e
Vinay-Tensor <Vinaynaidu3797@gmail.com>
2023-01-31 18:19:13 +0530

    new string

--- ReverseString.java
+++ ReverseString.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class ReverseString {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a string: ");
+        System.out.print("Enter a new string: ");
         String str = scanner.nextLine();
 
         String reversed = "";
